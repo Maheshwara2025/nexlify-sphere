25e6478bde02e08b2e79a81d05b9cc74288cac6d
srinivas ammu <nexlifynucleus@gmail.com>
2026-08-31 22:37:09 +0530

    Fix 500 internal error with client side rendering

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+const supabaseUrl = 'https://pxdavbhvpvkrxetctymo.supabase.co';
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4ZGF2Ymh2cHZrcnhldGN0eW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNzM0NDAsImV4cCI6MjEwMzc0OTQ0MH0.A77XfiDrjLJvTsK7Q06VopIG1nlF3fQhm2GhZmZ1BMc";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
