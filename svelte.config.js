099ce5e7ca344ee9861bc4a64f0cd4bceedc963e
srinivas ammu <nexlifynucleus@gmail.com>
2026-09-02 00:56:57 +0530

    fix: set SPA fallback to 404.html for cloudflare routing

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: '404.html', // 👈 Cloudflare Pages SPA కి ఇది ఖచ్చితంగా 404.html అయి ఉండాలి
 			precompress: false,
 			strict: false
 		})
