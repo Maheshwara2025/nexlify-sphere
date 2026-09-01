2cffa02adc35748dc40ad12f27893d1bfe9e67bd
srinivas ammu <nexlifynucleus@gmail.com>
2026-09-02 01:21:17 +0530

    fix: remove wrangler config and enforce clean static build

--- svelte.config.js
+++ svelte.config.js
@@ -9,13 +9,10 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: 'index.html',
 			precompress: false,
 			strict: false
-		}),
-		prerender: {
-			handleUnseenRoutes: 'ignore'
-		}
+		})
 	}
 };
 
